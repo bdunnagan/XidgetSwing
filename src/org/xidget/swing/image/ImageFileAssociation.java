8b91a714c4c9d87ec85c921f5a08719b35240d7e
Bob Dunnagan <bdunnagan@nc.rr.com>
2009-10-12 22:36:53 +0000

    Add file associations to JarCachingPolicy.

--- src/org/xidget/swing/image/ImageFileAssociation.java
+++ src/org/xidget/swing/image/ImageFileAssociation.java
@@ -4,7 +4,13 @@
  */
 package org.xidget.swing.image;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.ImageIcon;
 import org.xmodel.IModelObject;
 import org.xmodel.external.CachingException;
@@ -16,12 +22,54 @@ import org.xmodel.external.caching.IFileAssociation;
 public class ImageFileAssociation implements IFileAssociation
 {
   /* (non-Javadoc)
-   * @see org.xmodel.external.caching.IFileAssociation#apply(org.xmodel.IModelObject, java.io.File)
+   * @see org.xmodel.external.caching.IFileAssociation#apply(org.xmodel.IModelObject, java.lang.String, java.io.InputStream)
    */
-  public void apply( IModelObject parent, File file) throws CachingException
+  public void apply( IModelObject parent, String name, InputStream stream) throws CachingException
   {
-    ImageIcon image = new ImageIcon( file.getAbsolutePath());
-    parent.setValue( image);
+    try
+    {
+      byte[] content = readAll( stream);
+      ImageIcon image = new ImageIcon( content);
+      parent.setValue( image);
+    }
+    catch( IOException e)
+    {
+      throw new CachingException( "Unable to load image: "+name, e);
+    }
+  }
+  
+  /**
+   * Read the entire content of the stream and return the content.
+   * @param stream The stream.
+   * @return Returns the content of the stream.
+   */
+  private byte[] readAll( InputStream stream) throws IOException
+  {
+    ReadableByteChannel channel = Channels.newChannel( stream);
+    
+    int nread = 1, size = 0;
+    List<ByteBuffer> buffers = new ArrayList<ByteBuffer>();
+    while( nread > 0)
+    {
+      ByteBuffer buffer = ByteBuffer.allocate( 1 << 16);
+      nread = channel.read( buffer);
+      if ( nread > 0)
+      {
+        buffer.flip();
+        size += nread;
+        buffers.add( buffer);
+      }
+    }
+    
+    int offset = 0;
+    byte[] content = new byte[ size];
+    for( ByteBuffer buffer: buffers)
+    {
+      System.arraycopy( buffer.array(), 0, content, offset, buffer.remaining());
+      offset += buffer.remaining();
+    }
+    
+    return content;
   }
 
   /* (non-Javadoc)
