c50cacaffb61b94140e1aa72de49d81318d35c53
bdunnagan <bdunnagan@nc.rr.com>
2012-01-03 19:30:42 -0500

    Improvements to pie chart.

--- src/org/xidget/swing/chart/pie/PieChart.java
+++ src/org/xidget/swing/chart/pie/PieChart.java
@@ -6,11 +6,14 @@ package org.xidget.swing.chart.pie;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
 import java.awt.geom.Arc2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +32,7 @@ public class PieChart extends JPanel implements IPointsFeature
   {
     points = new ArrayList<Point>();
     stroke = new BasicStroke( (int)border, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+    shape = new Arc2D.Double();
   }
   
   /* (non-Javadoc)
@@ -48,11 +52,6 @@ public class PieChart extends JPanel implements IPointsFeature
   {
     if ( index < 0 || index > points.size()) return;
     
-    if ( points.size() == 0)
-    {
-      minX = maxX = point.coords[ 0];
-    }
-    
     if ( index > 0)
     {
       Point prev = points.get( index - 1);
@@ -62,48 +61,47 @@ public class PieChart extends JPanel implements IPointsFeature
       point.next = prev.next;
     }
     
-    if ( minX > point.coords[ 0]) minX = point.coords[ 0];
-    if ( maxX < point.coords[ 0]) maxX = point.coords[ 0];
-        
+    total += point.coords[ 0];
     points.add( index, point);
     
     repaint();
   }
 
   /* (non-Javadoc)
+   * @see org.xidget.ifeature.IPointsFeature#remove(int)
+   */
+  @Override
+  public void remove( int index)
+  {
+    total -= points.remove( index).coords[ 0];
+  }
+  
+  /* (non-Javadoc)
    * @see org.xidget.ifeature.IPointsFeature#update(org.xidget.graph.Point, int, double)
    */
   @Override
   public void update( Point point, int coordinate, double value)
   {
+    total -= point.coords[ coordinate];
     point.coords[ coordinate] = value;
-    if ( minX > point.coords[ 0] || maxX < point.coords[ 0])
-    {
-      findExtrema();
-    }
+    total += value;
     
     repaint();
   }
 
   /**
-   * Find the minimum and maximum values of the coordinates in the point list.
+   * Compute the fill and outline colors of the pie slices.
    */
-  private void findExtrema()
+  private void computeColors()
   {
-    if ( points == null || points.size() == 0)
+    drawColor = new ArrayList<Color>( points.size());
+    fillColor = new ArrayList<Color>( points.size());
+    for( int i=0; i<points.size(); i++)
     {
-      minX = 0;
-      maxX = 0;
-      return;
-    }
-    
-    minX = maxX = points.get( 0).coords[ 0];
-    for( int i=1; i<points.size(); i++)
-    {
-      Point point = points.get( i);
-      if ( point.coords[ 0] < minX) minX = point.coords[ 0];
-      if ( point.coords[ 0] > maxX) maxX = point.coords[ 0];
-    }
+      float hue = (float)i / points.size();
+      fillColor.add( Color.getHSBColor( hue, 0.9f, 1f));
+      drawColor.add( Color.getHSBColor( hue, 0.7f, 0.6f));
+    }   
   }
   
   /* (non-Javadoc)
@@ -114,53 +112,80 @@ public class PieChart extends JPanel implements IPointsFeature
   {
     super.paintComponent( g);
     
-    Graphics2D g2d = (Graphics2D)g;
+    if ( fillColor == null || fillColor.size() != points.size()) computeColors();
+    
+    Graphics2D g2d = (Graphics2D)g.create();
     g2d.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     g2d.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-    
-    Color oldColor = g2d.getColor();
-    Stroke oldStroke = g2d.getStroke();
-    
+
+    FontMetrics metrics = g2d.getFontMetrics();
     g2d.setStroke( stroke);
     
     int width = (int)Math.round( getWidth() - border * 2);
     int height = (int)Math.round( getHeight() - border * 2);
-    double radius = ((width < height)? width: height) / 2d - border - spread;
+    int size = ((width < height)? width: height);
+    double radius = size / 2d - border - spread;
 
-    double cx = getWidth() / 2;
-    double cy = getHeight() / 2;
+    double cx = getWidth() / 2d;
+    double cy = getHeight() / 2d;
+
+    Point2D.Double[] labelPoints = new Point2D.Double[ points.size()];
     
-    float hue = 0;
-    double range = (maxX - minX);
-    double curX = points.get( 0).coords[ 0];
     double angleD = 0;
-    for( int i=1; i<points.size(); i++)
+    for( int i=0; i<points.size(); i++)
     {
       Point point = points.get( i);
       
-      double extent = (point.coords[ 0] - curX) / range;
-      double extentD = extent * 360d;
+      double extentD = point.coords[ 0] / total * 360;
       
-      double spreadR = -(angleD + (extentD / 2)) * Math.PI / 180;
-      double spreadX = Math.cos( spreadR) * spread;
-      double spreadY = Math.sin( spreadR) * spread;
+      double midD = (angleD + (extentD / 2)) * Math.PI / 180;
+      double midUX = Math.cos( midD);
+      double midUY = Math.sin( midD);
       
-      Arc2D.Double arc = new Arc2D.Double();
-      arc.setArcByCenter( cx + spreadX, cy + spreadY, radius, angleD, extentD, Arc2D.PIE);
+      double d = spread / 2 / Math.sin( extentD * Math.PI / 360);
+      if ( d < 0) d = 0;
+      double spreadX = midUX * d;
+      double spreadY = midUY * d;
       
-      g2d.setColor( Color.getHSBColor( hue, 0.8f, 1f));
-      g2d.fill( arc);
+      shape.setArcByCenter( cx + spreadX, cy + spreadY, radius - d, -angleD, -extentD, Arc2D.PIE);
       
-      g2d.setColor( Color.getHSBColor( hue, 0.8f, 0.6f));
-      g2d.draw( arc);
+      g2d.setColor( fillColor.get( i));
+      g2d.fill( shape);
       
-      curX = point.coords[ 0];
+      g2d.setColor( drawColor.get( i));
+      g2d.draw( shape);
+      
+      if ( d < radius)
+      {
+        double labelRadius = (radius - d) / 2;
+        double lx = cx + spreadX + midUX * labelRadius;
+        double ly = cy + spreadY + midUY * labelRadius;
+        labelPoints[ i] = new Point2D.Double( lx, ly);
+      }
+      else
+      {
+        labelPoints[ i] = null;
+      }
+      
       angleD += extentD;
-      hue += extentD * Math.PI / 180;
-    }    
-    
-    g2d.setColor( oldColor);
-    g2d.setStroke( oldStroke);
+    }
+
+    g2d.setColor( Color.black);
+    for( int i=0; i<points.size(); i++)
+    {
+      Point point = points.get( i);
+      if ( point.label == null) point.label = "Label " + i;
+      
+      Point2D.Double labelPoint = labelPoints[ i];
+      if ( labelPoint == null) continue;
+
+      Rectangle2D bounds = metrics.getStringBounds( point.label, g2d);
+      
+      int lx = (int)Math.round( labelPoint.x - bounds.getWidth() / 2);
+      int ly = (int)Math.round( labelPoint.y + metrics.getDescent());
+      
+      g2d.drawString( point.label, lx, ly);
+    }
   }
 
   private double border = 1;
@@ -168,6 +193,9 @@ public class PieChart extends JPanel implements IPointsFeature
   private Stroke stroke;
   
   private List<Point> points;
-  private double minX;
-  private double maxX;
+  private double total;
+  
+  private Arc2D.Double shape = new Arc2D.Double();
+  private List<Color> fillColor;
+  private List<Color> drawColor;
 }
